6ec4dddc26de7e0d3585254be5f5ea96b91103ff
James Arthur <jamesarthur7@icloud.com>
2021-12-02 18:22:10 +0000

    day 2 star 2, not golfing this...

--- d2/d2.js
+++ d2/d2.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 var a1=fs.readFileSync('i.txt').toString().replace('\r','').split`\n`;
 var a2=['forward','up','down'];
 for(var k=a2.length;k--;){eval('var '+a2[k]+'=0');
-for(var j=a1.length;j--;){var a3=a1[j].toString().split` `;
-if(a3[0]==a2[k]){eval(a2[k]+'='+a2[k]+'+parseInt(a3[1])')}}}
+  for(var j=a1.length;j--;){var a3=a1[j].toString().split` `;
+      if(a3[0]==a2[k]){eval(a2[k]+'='+a2[k]+'+parseInt(a3[1])')}}}
 console.log(forward*(down-up))
---
+++ d2/d2b.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+var array = fs.readFileSync('i.txt').toString().replace('\r','').split`\n`;
+array.pop()
+
+up = 0;
+across = 0;
+aim = 0;
+
+for (var j = 0; j < array.length; j++) {
+  if (array[j].toString().split(" ")[0] == 'forward')
+    {
+      across = across + parseInt(array[j].toString().split(" ")[1]);
+      up = up + aim * parseInt(array[j].toString().split(" ")[1]);
+    }
+  if (array[j].toString().split(" ")[0] == 'up')
+    {aim = aim - parseInt(array[j].toString().split(" ")[1]);}
+  if (array[j].toString().split(" ")[0] == 'down')
+    {aim = aim + parseInt(array[j].toString().split(" ")[1])}
+}
+console.log(up * across)
