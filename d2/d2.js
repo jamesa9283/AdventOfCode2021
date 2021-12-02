77abad8710eb962b115ddf5c4243433b929594c3
James Arthur <jamesarthur7@icloud.com>
2021-12-02 18:06:15 +0000

    full golf

--- d2/d2.js
+++ d2/d2.js
@@ -1,14 +1,7 @@
 const fs = require('fs')
-var array = fs.readFileSync('./input.txt').toString().replace('\r', '').split`\n`;
-var up = forward = 0;
-p = parseInt;
-for (var j=array.length; j--; ) {
-  var array2 = array[j].toString().split` `;
-  if (array2[0] == 'forward')
-    {forward = forward + p(array2[1]);}
-  if (array2[0] == 'up')
-    {up = up - p(array2[1]);}
-  if (array2[0] == 'down')
-    {up = up + p(array2[1])}
-}
-console.log(up * forward)
+var a1=fs.readFileSync('i.txt').toString().replace('\r','').split`\n`;
+var a2=['forward','up','down'];
+for(var k=a2.length;k--;){eval('var '+a2[k]+'=0');
+for(var j=a1.length;j--;){var a3=a1[j].toString().split` `;
+if(a3[0]==a2[k]){eval(a2[k]+'='+a2[k]+'+parseInt(a3[1])')}}}
+console.log(forward*(down-up))
