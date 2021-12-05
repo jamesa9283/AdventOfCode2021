3d664ce9fd7df34affcbe99c9b9772cbfe9abb26
James Arthur <jamesarthur7@icloud.com>
2021-12-05 22:43:20 +0000

    I DID THE FIRST STAR!

--- d5/d5.js
+++ d5/d5.js
@@ -4,12 +4,7 @@ var a1=fs.readFileSync('test.txt').toString().split`\r\n`; // takes in data and 
 var lines = [];
 var points = [];
 
-function ArrayEqual(arr1, arr2) {
-  for (a=0; a < arr1.length; a++) {
-      if (arr1[a] != arr2[a]) { return false}
-  }
-  return true
-}
+pts = [];
 
 pI = parseInt
 // console.log(a1)
@@ -17,41 +12,46 @@ for (i=0; i < a1.length; i++) {
   line = a1[i].split(' -> ');
   if (line[0].split(',')[0] == line[1].split(',')[0]) {
     lines.push([(line[0].split(',')), (line[1].split(','))]);
+    pts.push(line[0].split(','), line[1].split(','));
   }
   if (line[0].split(',')[1] == line[1].split(',')[1]) {
     lines.push([(line[0].split(',')), (line[1].split(','))]);
+    pts.push((line[0].split(',')), (line[1].split(',')));
   }
 }
-console.log(lines)
+console.log(line)
+// console.log(pts.flat())
+// console.log(Math.max(...pts.flat()))
+
+var value = 0; // by default
+var board = [...Array(1 + Math.max(...pts.flat()))].map(e => Array(1 + Math.max(...pts.flat())).fill(value));
+// console.log(board)
+
+for (i = 0; i < lines.length; i++) {
+  // console.log(lines[i][0])
+  // console.log(lines[i][j][0], lines[i][j][1])
+  if (lines[i][0][0] - lines[i][1][0] <= 0) { var x0 = pI(lines[i][0][0]); var x1 = pI(lines[i][1][0])}
+  if (lines[i][0][0] - lines[i][1][0] > 0) { var x1 = pI(lines[i][0][0]); var x0 = pI(lines[i][1][0])}
+  if (lines[i][0][1] - lines[i][1][1] <= 0) { var y0 = pI(lines[i][0][1]); var y1 = pI(lines[i][1][1])}
+  if (lines[i][0][1] - lines[i][1][1] > 0) { var y1 = pI(lines[i][0][1]); var y0 = pI(lines[i][1][1])}
+  console.log(x0, y0, x1, y1)
+  for (k=x0; k <= x1; k++) {
+    console.log('yes')
+    for (l=y0; l <= y1; l++ ) {
+      console.log('Changed')
+      board[k][l] = board[k][l] + 1;
+    }
+  }
+}
 
 var score = 0;
 
-for (i = 0; i < lines.length; i++) {
-  console.log('l1: ' + lines[i]);
-  var x0 = pI(lines[i][0][0]); y0 = pI(lines[i][0][1]); x1 = pI(lines[i][1][0]); y1 = pI(lines[i][1][1]);
-  var m = (y1 - y0)/(x1 - x0);
-  console.log(m);
-  for (j = i+1; j < lines.length; j++) {
-    var x0Bar = lines[j][0][0]; y0Bar = lines[j][0][1]; x1Bar = lines[j][1][0]; y1Bar = lines[j][1][1];
-    console.log('l2: ' + lines[j]);
-    var mBar = (y1Bar - y0Bar)/(x1Bar - x0Bar);
-    var x = (y0 - y0Bar - m * x0 + mBar * x0Bar)/(mBar - m);
-    var y = y0 + m * (x - x0);
-    if ((!isFinite(m) && Math.abs(mBar) == 0)) {
-      if (((x0Bar <= x0 && x0 <= x1Bar) && (y0Bar <= y0 && y0 <= y1Bar)) || (x1Bar <= x0 && x0 <= x0Bar) && (y1Bar <= y0 && y0 <= y0Bar)) {
-        score = score + 1;
-        console.log('contacts')
-      }
-      // console.log('constant');
-    }
-    if (!isFinite(mBar) && Math.abs(m) == 0) {
-      if ((x0 <= x0Bar && x0Bar <= x1) || (x1 <= x0Bar && x0Bar <= x0)) {
-        score = score + 1;
-        console.log('contact')
-      }
-      // console.log('constant');
-    }
+for (i = 0; i < board.length; i++) {
+  for (j = 0; j < board[0].length; j++)
+  if (board[i][j] > 1) {
+    score = score + 1
   }
 }
 
+console.log(board)
 console.log(score)
