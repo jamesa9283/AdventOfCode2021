82b49858a1c1c1b7df690d088cf8b6e4c5cb0d5d
James Arthur <jamesarthur7@icloud.com>
2021-12-09 19:38:23 +0000

    added some updates

--- d1/d1b.js
+++ d1/d1b.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
 
+function countDepth(addData) {
+  var counter = 0;
+  for (var j = 0; j < addData.length; j++) {
+    if (addData[j-1] < addData[j]) {counter = counter + 1; }
+  }
+   return counter;
+}
+
 var array = fs.readFileSync('./input.txt').toString().split("\n");
 for (var i = 0; i < array.length; i++) {
   array[i] = array[i].replace('\r', '');
@@ -13,9 +21,5 @@ for (var j = 0; j < array.length - 2; j++) {
   addData.push(parseInt(array[j])+parseInt(array[j+1])+parseInt(array[j+2]));
 }
 console.log(addData);
-
-var counter = 0;
-for (var j = 0; j < addData.length; j++) {
-  if (addData[j-1] < addData[j]) {counter = counter + 1; }
-}
-console.log(counter);
+console.log(countDepth(array)+1)
+console.log(countDepth(addData))
--- d2/d2b.js
+++ d2/d2b.js
@@ -9,10 +9,8 @@ aim = 0;
 
 for (var j = 0; j < array.length; j++) {
   if (array[j].toString().split(" ")[0] == 'forward')
-    {
-      across = across + parseInt(array[j].toString().split(" ")[1]);
-      up = up + aim * parseInt(array[j].toString().split(" ")[1]);
-    }
+    { across = across + parseInt(array[j].toString().split(" ")[1]);
+      up = up + aim * parseInt(array[j].toString().split(" ")[1]);}
   if (array[j].toString().split(" ")[0] == 'up')
     {aim = aim - parseInt(array[j].toString().split(" ")[1]);}
   if (array[j].toString().split(" ")[0] == 'down')
